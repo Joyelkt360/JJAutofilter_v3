e9fbf5a1d8a0523e7b57fca7a722ad278af76e6f
joyel <89131252+Joyelkt360@users.noreply.github.com>
2022-03-03 11:24:50 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -63,7 +63,7 @@ async def filter(client, message):
                     [InlineKeyboardButton(text=f"{filename}",callback_data=f"pr0fess0r_99#{file_id}")]
                     )
         else:
-            await client.send_text(chat_id=message.from_user.id, (text='NO FILE IN MY DATABASE'))
+            await client.send_text(chat_id=message.from_user.id, text='NO FILE IN MY DATABASE')
             return
 
         if not btn:
